cbbf482a481eed27910cb864a8172cc7ba0f2ad9
Daniel BS <danielsalesianosdosa@gmail.com>
2024-10-20 23:09:46 +0200

    20oct

---
+++ Curso Java Verano 2024/Tema 8/src/Pruebas/Ejemplo_File.java
@@ -0,0 +1,34 @@
+package Pruebas;
+import java.io.*;
+
+public class Ejemplo_File {
+
+	public static void main(String[] args) {
+		
+		File fichero = new File("src/Prueba/fichero.txt");
+			
+		try {
+			
+			System.out.println(fichero.createNewFile() ? "Se he creado el fichero" : "Ya existe"); 
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	/*		try {
+				if(fichero.createNewFile())
+					System.out.println("Se ha creado el fichero");
+				else
+					System.out.println("Ya existe");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			*/
+			
+
+		System.out.println(fichero.getPath());
+
+	}
+
+}
